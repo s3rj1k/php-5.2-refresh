35ab875a7a170ad7c0839480d6ba941a73e6ef71
s3rj1k <evasive.gyron@gmail.com>
2017-06-08 16:40:51 +0300

    CVE-2010-0397

--- ext/xmlrpc/xmlrpc-epi-php.c
+++ ext/xmlrpc/xmlrpc-epi-php.c
@@ -736,10 +736,15 @@ zval* decode_request_worker (zval* xml_in, zval* encoding_in, zval* method_name_
 			if(method_name_out) {
 				method_name = XMLRPC_RequestGetMethodName(response);
 				if (method_name) {
-					zval_dtor(method_name_out);
-					Z_TYPE_P(method_name_out) = IS_STRING;
-					Z_STRVAL_P(method_name_out) = estrdup(method_name);
-					Z_STRLEN_P(method_name_out) = strlen(Z_STRVAL_P(method_name_out));
+					method_name = XMLRPC_RequestGetMethodName(response);
+					if (method_name) {
+						zval_dtor(method_name_out);
+						Z_TYPE_P(method_name_out) = IS_STRING;
+						Z_STRVAL_P(method_name_out) = estrdup(method_name);
+						Z_STRLEN_P(method_name_out) = strlen(Z_STRVAL_P(method_name_out));
+					} else {
+						retval = NULL;
+					}
 				} else if (retval) {
 					zval_ptr_dtor(&retval);
 					retval = NULL;
