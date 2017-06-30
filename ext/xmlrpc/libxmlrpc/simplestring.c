4698c6ca0c2a94892ce91d4e33d26016defc6071
s3rj1k <evasive.gyron@gmail.com>
2017-07-01 01:33:08 +0300

    CVE-2016-6292

--- ext/xmlrpc/libxmlrpc/simplestring.c
+++ ext/xmlrpc/libxmlrpc/simplestring.c
@@ -197,6 +197,10 @@ void simplestring_addn(simplestring* target, const char* source, size_t add_len)
          simplestring_init_str(target);
       }
 
+#ifndef SIZE_MAX 
+# define SIZE_MAX ((size_t) -1) 
+#endif
+
       if((SIZE_MAX - add_len) < target->len || (SIZE_MAX - add_len - 1) < target->len) {
     	  /* check for overflows, if there's a potential overflow do nothing */
     	  return;
